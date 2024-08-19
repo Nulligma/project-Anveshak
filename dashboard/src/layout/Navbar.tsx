1d50a2ea5057ca9b127fd3a787450d64d16f82ec
Nulligma <shantanu@nulligma.com>
2024-08-19 18:20:52 +0530

    sidebar nav icons ui

--- dashboard/src/layout/Navbar.tsx
+++ dashboard/src/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // styles
 import styles from "../assets/styles/modules/nav.module.css";
@@ -13,10 +13,17 @@ import moonSvg from "../assets/svg/icon-moon.svg";
 import useDarkModeToggle from "../hooks/useDarkMode";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { Link } from "react-router-dom";
+import {
+  BanknotesIcon,
+  ChartBarSquareIcon,
+  NewspaperIcon,
+  ChatBubbleOvalLeftIcon,
+} from "@heroicons/react/24/outline";
 
 const Navbar = () => {
   const { isDarkMode, toggleTheme, setDarkMode } = useDarkModeToggle();
   const [value, setValue] = useLocalStorage("isDarkMode", isDarkMode);
+  const [navSelected, setNavSelected] = useState("articles");
 
   const handleToggleDarkMode = () => {
     toggleTheme();
@@ -33,6 +40,49 @@ const Navbar = () => {
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
       </div>
+
+      <ul className={styles.iconRoutes}>
+        <li
+          id="articles"
+          className={
+            navSelected === "articles" ? styles.selected : styles.normalRoute
+          }
+          onClick={() => setNavSelected("articles")}
+        >
+          <NewspaperIcon />
+          <span>Articles</span>
+        </li>
+        <li
+          id="cards"
+          className={
+            navSelected === "cards" ? styles.selected : styles.normalRoute
+          }
+          onClick={() => setNavSelected("cards")}
+        >
+          <BanknotesIcon />
+          <span>Cards</span>
+        </li>
+        <li
+          id="analytics"
+          className={
+            navSelected === "analytics" ? styles.selected : styles.normalRoute
+          }
+          onClick={() => setNavSelected("analytics")}
+        >
+          <ChartBarSquareIcon />
+          <span>Analytics</span>
+        </li>
+        <li
+          id="chat"
+          className={
+            navSelected === "chat" ? styles.selected : styles.normalRoute
+          }
+          onClick={() => setNavSelected("chat")}
+        >
+          <ChatBubbleOvalLeftIcon />
+          <span>Chat</span>
+        </li>
+      </ul>
 
       <div className={styles.wrapper}>
         <div className={styles.darkmodeToggle} onClick={handleToggleDarkMode}>
